2baef291fc53c061aca4102cb31dee43e445e166
Edgar Martinez <eddmtz96@gmail.com>
2024-11-25 10:48:33 -0600

    change SSH keys name

--- generate_keys.sh
+++ generate_keys.sh
@@ -1,4 +1,7 @@
-ssh-keygen -t rsa -N "" -f ansible
-
-mkdir keys
-mv ansible ansible.pub keys/
+if ssh-keygen -t rsa -N "" -f ssh_ansible > /dev/null 2>&1; then
+    echo "SSH keys successfully generated"
+    mkdir keys
+    mv ssh_ansible ssh_ansible.pub keys/
+else
+    echo "Failed to generate SSH keys"
+fi
